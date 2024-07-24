fb6d3b19f43c79b943f313025b9ca8ea3553d5cb
Arshad <arshad.mehmood@securetech-consultancy.com>
2024-07-24 22:36:25 +0500

    Changes: Updated with some changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,9 @@
-const { defineConfig } = require("cypress");
-const cucumber = require('cypress-cucumber-preprocessor').default
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-    projectId: "xgvkov",
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
-
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/Features/*.feature",
     baseUrl: 'https://www.bilkamadudafhuset.dk/',
-
   },
   pageLoadTimeout: 1000000,
   video: false,
@@ -19,8 +11,5 @@ module.exports = defineConfig({
   requestTimeout: 9000000,
   responseTimeout: 9000000,
     "chromeWebSecurity": false,
-    "chromeWebSecurity": false,
     "redirectionLimit": 1000
-   
-
-});
+};
